3cb8d7f608a81b309f9cb20660df637557869077
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-01-19 09:06:10 +0700

    Merapihkan program

--- main.mjs
+++ main.mjs
@@ -7,6 +7,47 @@ const input = readlinePromises.createInterface({
     output:process.stdout
 })
 
-const jawaban = await input.question("Kamu ingin layanan apa? ");
-console.log(`Layanan yang kamu pilih adalah ${jawaban}`);
+let condition = true
+
+const opsi = ["Versi","platform","mesin","nama host","perilisan", "tipe"];
+
+
+while(condition){
+    console.log("Daftar Layanan : ")
+
+
+    for(let i in opsi){
+    console.log(`${Number(i)+1}. ${opsi[i]}`);
+    }
+    console.log("Ketik \'Keluar\' untuk keluar dari program");
+
+
+    const jawaban = await input.question("Kamu ingin layanan apa? ");
+    
+
+
+    if(jawaban==1 || jawaban.toLowerCase()===opsi[0].toLowerCase()){
+        console.log(`Informasi mengenai Versi : \n ${os.version()} \n\n`);
+    }else if(jawaban==2 || jawaban.toLowerCase()==opsi[1].toLowerCase()){
+        console.log(`Informasi mengenai platform : \n ${os.platform()} \n\n`);
+    }else if(jawaban==3 || jawaban.toLowerCase()==opsi[2].toLowerCase()){
+        console.log(`Informasi mengenai mesin : \n ${os.machine()} \n\n`);
+    }
+    else if(jawaban==4 || jawaban.toLowerCase()==opsi[3].toLowerCase()){
+        console.log(`Informasi mengenai nama host : \n ${os.hostname()} \n\n`);
+    }
+    else if(jawaban==5 || jawaban.toLowerCase()==opsi[4].toLowerCase()){
+        console.log(`Informasi mengenai perilisan : \n ${os.release()} \n\n`);
+    }
+    else if(jawaban==6 || jawaban.toLowerCase()==opsi[5].toLowerCase()){
+        console.log(`Informasi mengenai tipe : \n ${os.type()} \n\n`);
+    }else if(jawaban.toLowerCase()==="keluar".toLowerCase()){
+        console.log("Anda memilih keluar dari program");
+        condition=false;
+    }else{
+        console.log("Opsi tidak dikenal \n\n");
+    }
+    
+}
+
 input.close();
